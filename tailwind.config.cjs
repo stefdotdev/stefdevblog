ee3d409493783e7142c3260c04408f0186fa169c
Stefan Bullivant <stefan@sbmbpro.local>
2023-08-19 22:30:05 +1200

    Blog view page working

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,9 +3,6 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  options: {
-    safelist: ["green"],
-  },
   theme: {
     extend: {
       fontFamily: {
